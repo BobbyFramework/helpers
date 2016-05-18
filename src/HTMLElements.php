e17188c05f62e3e889a60f834a5984635db3fe72
nicolas <n.wallerand@gmail.com>
2016-05-18 12:56:15 +0200

    first commit

---
+++ src/HTMLElements.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace BobbyFramework\Session;
+
+class HTMLElements {
+
+    public static function arrayToAttributes(array $attributes)
+    {
+        //Build a list of single attributes first
+        $attributeList = array();
+        foreach ($attributes as $key => $value) {
+            // If the value is not false add the attribute. This allows attributes to not be shown.
+            if ($value !== false) {
+                if (is_string($key)) {
+                    $attributeList[] = htmlspecialchars($key) . '="' . htmlspecialchars($value) . '"';
+                } else {
+                    $attributeList[] = $value;
+                }
+            }
+        }
+        return implode(' ', $attributeList);
+    }
+}
