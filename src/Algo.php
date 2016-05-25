0c32277fb501557e8fe2b01220a26a0fcb9b755a
nicolas <n.wallerand@gmail.com>
2016-05-25 14:06:21 +0200

    Update Algo.php

--- src/Algo.php
+++ src/Algo.php
@@ -4,5 +4,27 @@ namespace BobbyFramework\Helpers;
 
 class Algo
 {
+ static public function checkLuhn($val) {
 
+        $len   = strlen($val);
+        $total = 0;
+        for ($i = 1; $i <= $len; $i++) {
+            $j = substr($val, -$i, 1);
+            if ($i % 2 == 0) {
+                $total += 2 * $j;
+                if ((2 * $j) >= 10) {
+                    $total -= 9;
+                }
+            }
+            else {
+                $total += $j;
+            }
+        }
+        if ($total % 10 == 0) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
 }
